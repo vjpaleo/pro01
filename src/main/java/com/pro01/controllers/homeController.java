f258a796baa49daf17f10198b4613437305fd79c
Vijay Singh <vsingh@progrexion.com>
2017-01-07 23:23:59 -0700

    add simple random quote and first controller.

---
+++ src/main/java/com/pro01/controllers/homeController.java
@@ -0,0 +1,31 @@
+package com.pro01.controllers;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class homeController {
+	
+	private String[] quotes = {
+			"If you don't like something, change it. If you can't change it, change your attitude. Don't complain. -- Maya Angelou",
+			"Good, better, best. Never let it rest. 'Til your good is better and your better is best. -- St. Jerome",
+			"The good life is one inspired by love and guided by knowledge. -- Bertrand Russell",
+			"I am as bad as the worst, but, thank God, I am as good as the best. -- Walt Whitman"
+			};
+	
+	@RequestMapping(value="/getQuote")
+	public String getRandomQuote(Model model) {
+		
+		int rand = new Random().nextInt(quotes.length);
+		
+		String currentQuote = this.quotes[rand];
+		
+		model.addAttribute("randomQuote", currentQuote);
+		
+		return "quote";
+	}
+
+}
